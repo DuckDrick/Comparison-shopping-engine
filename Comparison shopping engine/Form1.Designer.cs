12232a9864ad1f4a7ae697bd3868f1e15b2d8471
Bertašius Andrius <andrius.bertasius@mif.stud.vu.lt>
2020-09-19 16:40:37 +0300

    Simple scraper that gets data from rde.lt store

--- Comparison shopping engine/Form1.Designer.cs
+++ Comparison shopping engine/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.search = new System.Windows.Forms.TextBox();
+            this.scrape = new System.Windows.Forms.Button();
+            this.results = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(106, 46);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(294, 20);
+            this.search.TabIndex = 0;
+            // 
+            // scrape
+            // 
+            this.scrape.Location = new System.Drawing.Point(406, 46);
+            this.scrape.Name = "scrape";
+            this.scrape.Size = new System.Drawing.Size(294, 20);
+            this.scrape.TabIndex = 1;
+            this.scrape.Text = "Scrape";
+            this.scrape.UseVisualStyleBackColor = true;
+            this.scrape.Click += new System.EventHandler(this.Scrape);
+            // 
+            // results
+            // 
+            this.results.Location = new System.Drawing.Point(106, 81);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(594, 395);
+            this.results.TabIndex = 2;
+            this.results.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(848, 488);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.scrape);
+            this.Controls.Add(this.search);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.Button scrape;
+        private System.Windows.Forms.RichTextBox results;
     }
 }
 
---
+++ Comparison shopping engine/Scraper.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class Scraper
+{
+    private RichTextBox results;
+    public Scraper(string url, RichTextBox results)
+    {
+        t(url);
+        this.results = results;
+    }
+
+    private static string pages = null;
+
+
+    private async void t(string url)
+    {
+        await GetHtmlAsync(url + "1");
+        int s = 1;
+        if (!(pages is null))
+        {
+            s = int.Parse(pages);
+        }
+        for (int i = 2; i <= s; i++)
+        {
+            await GetHtmlAsync(url + i.ToString());
+        }
+    }
+
+    private async Task GetHtmlAsync(string url)
+    {
+
+        var httpClient = new HttpClient();
+        var html = await httpClient.GetStringAsync(url);
+
+        var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+        htmlDocument.LoadHtml(html);
+
+
+        if (pages is null)
+        {
+            pages = htmlDocument.DocumentNode.Descendants("div")
+            .Where(node => node.GetAttributeValue("class", "")
+            .Equals("pages")).LastOrDefault().Descendants("a").LastOrDefault().InnerText;
+            Console.WriteLine(pages);
+        }
+
+        var ProductList = htmlDocument.DocumentNode.Descendants("div")
+            .Where(node => node.GetAttributeValue("class", "")
+            .Equals("product_box_div")).ToList();
+
+        Regex rgx = new Regex("[^(\\d+\\.\\d+)]");
+        foreach (var Product in ProductList)
+        {
+            results.AppendText(Product.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("product_name")).FirstOrDefault().InnerText + " " +
+                rgx.Replace(Product.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Contains("product_price")).FirstOrDefault().InnerText, "") + "\n");
+        }
+
+
+    }
+}
