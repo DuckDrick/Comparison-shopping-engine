830e8aea22d968574dfd07c9ab7d7b7b8e423fe7
Gelčys Juozapas <juozapas.gelcys@mif.stud.vu.lt>
2020-10-13 15:32:25 +0300

    .

--- Comparison shopping engine/Form1.cs
+++ Comparison shopping engine/Form1.cs
@@ -75,10 +75,10 @@ namespace Comparison_shopping_engine
 
                 productListView.Items.Clear();
                 PopulateProductListView();
-                //backgroundWorker1.RunWorkerAsync(argument: search.Text);
-                //backgroundWorker2.RunWorkerAsync(argument: search.Text);
-                //backgroundWorker3.RunWorkerAsync(argument: search.Text);
-                //backgroundWorker4.RunWorkerAsync(argument: search.Text);
+                backgroundWorker1.RunWorkerAsync(argument: search.Text);
+                backgroundWorker2.RunWorkerAsync(argument: search.Text);
+                backgroundWorker3.RunWorkerAsync(argument: search.Text);
+                backgroundWorker4.RunWorkerAsync(argument: search.Text);
                 backgroundWorker5.RunWorkerAsync(argument: search.Text);
             }
             else
